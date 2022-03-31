752b6a3e65695669a4afbb0b4d8b47a72296e56d
Prince Roy <princeku.ray143@gmail.com>
2022-03-31 23:41:07 +0530

    added inputs checks

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
-const firstName = document.getElementById("fname");
-const lastName = document.getElementById("lname");
+const form = document.querySelector(".form");
+const inputs = document.getElementsByClassName("inputs");
 const email = document.getElementById("email");
-const password = document.getElementById("password");
 
+function isValidEmail(userEmail) {
+    const regexEmail =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    return regexEmail.test(userEmail);
+}
 
-document.getElementById('submitBtn').addEventListener("submit", (e) => {
-    validate();
+const invalid = input => {
+    input.placeholder = "";
+    input.classList.add("invalid");
+}
+
+const valid = input => {
+    input.placeholder = "";
+    input.classList.remove("invalid");
+}
+
+form.addEventListener("submit", e => {
     e.preventDefault();
+
+    inputs.forEach(input => {
+        const inputVal = input.value.trim();
+
+        inputVal ? valid(input) : invalid(input);
+
+        if (input === email) isValidEmail(input.value) && valid(input);
+    })
 });
